ed332a9521c8ebb362d1dcc2082065b00be9e283
maxrobinso <maxrobinsons.inbox@gmail.com>
2021-02-22 20:50:02 +0300

    added pendings

--- src/sushi/utils.js
+++ src/sushi/utils.js
@@ -74,7 +74,7 @@ export const getPoolWeight = async (masterChefContract, pid) => {
 }
 
 export const getEarned = async (masterChefContract, pid, account) => {
-  return masterChefContract.methods.pendingSushi(pid, account).call()
+  return masterChefContract.methods.pendingUFO(pid, account).call()
 }
 
 export const getTotalLPWethValue = async (
